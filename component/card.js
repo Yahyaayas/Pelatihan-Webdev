a4e82ac37edaf9c1f81ef479c2035d8d48b33f27
yahya ayas <jamehsgaming@gmail.com>
2023-05-06 16:50:24 +0700

    try next js

---
+++ component/card.js
@@ -0,0 +1,9 @@
+export default function Card({isiteks}) {
+    return(
+        <>
+            <div className="kartu">
+            <h1>{isiteks}</h1>
+            </div>
+        </>
+    );
+}
---
+++ pages/test/index.js
@@ -0,0 +1,14 @@
+import Card from "@/component/card";
+
+export default function Test() {
+  
+  return(
+    <> 
+        <h2>kepala bapakkau!</h2>
+        <Card isiteks="anjing"/>
+        <Card isiteks="kucing"/>
+        <Card isiteks="kambing"/>
+        <div class="kartu"></div>
+    </>
+  );
+}
